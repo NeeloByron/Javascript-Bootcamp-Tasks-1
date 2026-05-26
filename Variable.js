ccd06e411b00ce8c98536706e89cdfe1c3e16a09
NeeloByron <99469633+NeeloByron@users.noreply.github.com>
2026-05-26 04:51:33 +0200

    Complete Variable file

---
+++ Variable.js
@@ -0,0 +1,8 @@
+//1. Declare favoriteColour & assign favoriteColor
+let favoriteColour = 'Red'
+//2. Print value of favoriteColor
+console.log(favoriteColour);
+//3. Reassign to different color
+favoriteColour = 'Blue'
+//4. Print value of new favoriteColour
+console.log(favoriteColour);
