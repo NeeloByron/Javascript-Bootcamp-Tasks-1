e1e17e6271164786b10cabfccaeb044121f09840
NeeloByron <99469633+NeeloByron@users.noreply.github.com>
2026-05-26 12:32:36 +0200

    Moved all variables in variable file and rename my files correctly

--- Variable-Declaration.js
+++
@@ -1,8 +0,0 @@
-//1. Declare name
-let name = "Byron"
-//2. Declare age
-let age = 29
-//3. Declare isStudent & boolean assigned 
-let isStudent = true
-//4. Print all 3 variables
-console.log(name, age, isStudent)
