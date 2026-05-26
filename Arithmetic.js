9b480638b4dfa66be86dbccd2b6942775476b4ba
NeeloByron <99469633+NeeloByron@users.noreply.github.com>
2026-05-26 10:05:27 +0200

    Complete Arithmetic file

---
+++ Arithmetic.js
@@ -0,0 +1,13 @@
+//1. Declare two variables 'num1' and 'num2' and  assign them any two numbers
+let num1 = 8;
+let num2 = 4;
+//2. Declare variables to store the sum, difference, product and quotient of num1 & num2
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+//3. Print the results of these arithmetic operations.
+console.log('Sum:', sum)
+console.log('Difference:', difference)
+console.log('Product:', product)
+console.log('Quotient:', quotient)
